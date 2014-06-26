95d64291ae3f5ba5c172f78392cb42f95ccaa450
irthos <irth03@gmail.com>
2014-06-26 01:24:19 -0700

    input

--- directive/views/mngrInput/mngrInput.js
+++ directive/views/mngrInput/mngrInput.js
@@ -1,31 +1,36 @@
-angular.module('mngr').directive('mngrInput', function() {
-	return {
-		restrict: 'E',
-		replace: true,
-		controller:function($scope){
-			var properties=[
-				'defaultValue',
-				'disabled',
-				'form'
-			];
-			var inputs=[
-				{type:'text'},
-				{type:'number'},
-				{type:'checkbox'},
-				{type:'select'},
-				{type:'range'},
-				{type:'file'},
-				{type:'password'},
-				{type:'radio'}
-			];
-		},
-		scope: {
+angular.module("mngr").directive("mngrInput", function($compile){
+		return {
+			restrict:"E",
+			controller:function($scope){
+				var properties=[
+					'defaultValue','disabled','form','list','max','min','name','step','type','value','size'
+				];
+				var inputs=[
+					{type:'text'},
+					{type:'number'},
+					{type:'checkbox'},
+					{type:'select'},
+					{type:'range'},
+					{type:'file'},
+					{type:'password'},
+					{type:'radio'}
+				];
+			},
+			scope:{element:"=", properties:"=", options:"=", type:"=", model:"=ngModel", placeholder:'@', name:'@'},
+			link:function(scope, iElem, iAttrs) {
 
-		},
-		templateUrl: 'directive/views/mngrInput/mngrInput.html',
-		link: function(scope, element, attrs, fn) {
-
-
-		}
-	};
-});
+				var angElement = angular.element(document.createElement(scope.element));
+				angElement.attr('name', scope.name);
+				angElement.attr('properties', scope.properties);
+				angElement.attr('options', scope.options);
+				angElement.attr('id', scope.name);
+				angElement.attr('ng-model', 'model');
+				if(scope.element === 'input'){
+					angElement.attr('type', scope.type);
+				}
+				$compile(angElement)(scope);
+				iElem.append(angElement);
+			}
+		};
+	}
+);
