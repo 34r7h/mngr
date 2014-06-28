595f8a6c2b04d2ccf77ee77cce70c9b2f3b52bbb
Pat Long <plong00@gmail.com>
2014-06-27 21:48:13 -0700

    moved data initialization out of data service and into app run, dataLoader

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ angular.module('mngr', ['ui.utils','ui.router','ngAnimate', 'firebase', 'mngr.ut
 
 angular.module('mngr').config(function($stateProvider, $urlRouterProvider) {
 	$stateProvider.state('main', {
-		url:'*path?role&preferences&history&store&overlay&main&leftbar&rightbar&event&info&inventory&activity&message&order&product',
+		url:'*path',
 		reloadOnSearch: false,
 		controller: ['$scope','$location','ui', 'models', 'data', 'api' ,function($scope, $location, ui, models, data, api){
 			ui.loadState(angular.copy($location.path()), angular.copy($location.search()));
 			$scope.$on('$locationChangeStart', function(){
 				ui.loadState(angular.copy($location.path()), angular.copy($location.search()));
 			});
-			$scope.api = api;
-			$scope.ui = ui;
-			$scope.models = models;
-			$scope.data = data;
 
+            $scope.api = api;
+            $scope.ui = ui;
+            $scope.models = models;
+            $scope.data = data;
 		}],
 		template: '<main></main>'
 	});
@@ -22,7 +22,69 @@ angular.module('mngr').config(function($stateProvider, $urlRouterProvider) {
 
 });
 
-angular.module('mngr').run(function($rootScope) {
+angular.module('mngr').run(function($rootScope, data, $firebase, Firebase, $filter) {
+    var dataLoader = {
+        // ecodocs: initialize the data
+        init: function (types) {
+            // by default load everything
+            types = (angular.isDefined(types)?(angular.isArray(types)?types:[types]):data.types);
+            this.initObjects(types);
+            this.initFires(types);
+            this.initArrays(types);
+        },
+
+        // ecodocs: initialize the container objects for each data type
+        initObjects: function(types) {
+            angular.forEach(types, function(type){
+                data[type] = {
+                    ref: new Firebase("https://mngr.firebaseio.com/"+type),
+                    fire: null,
+                    array: []
+                };
+            });
+        },
+
+        // ecodocs: initialize the $firebase bindings for each data type
+        initFires: function (types) {
+            angular.forEach(types, function(type){
+                if(data[type].ref){
+                    data[type].fire = $firebase(data[type].ref);
+                }
+            });
+        },
+
+        // ecodocs: initialize the arrays for each data type
+        initArrays: function (types) {
+            angular.forEach(types, function(type) {
+                if(data[type] && data[type].fire && angular.isFunction(data[type].fire.$on)){
+                    data[type].fire.$on('value', function(){
+                        dataLoader.fireToArray(type);
+                    });
+                }
+            });
+        },
+
+        // ecodocs: convert a $firebase refernce to an array (callback for data[type].fire.$on('value', function)
+        fireToArray: function(type){
+            data[type].array = $filter('orderByPriority')(data[type].fire);
+            console.log(type+' loaded');
+        }
+    };
+    dataLoader.init([
+        'products',
+        'orders',
+        'users',
+        'events',
+        'messages',
+        'notices',
+        'contents',
+        'notes',
+        'shops',
+        'ui',
+        'settings',
+        'roles'
+    ]);
+
 
 
     $rootScope.safeApply = function(fn) {
--- service/data.js
+++ service/data.js
@@ -1,107 +1,13 @@
-angular.module('mngr').factory('data',function($firebase, Firebase, $firebaseSimpleLogin, $filter) {
+angular.module('mngr').factory('data',function(Firebase, $firebaseSimpleLogin) {
 
 	var data = {
         path: '/',
         params: {},
         user: {
-            account: null,  // firebase authentication account
-            credentials: {
-                email: '',
-                password: '',
-                passConfirm: '',
-                recover: false
-            },
             profile: null,  // user profile
             auth: $firebaseSimpleLogin(new Firebase("https://mngr.firebaseio.com/"))
-        },
-
-        // ecodocs: list of data types the service provides
-        types: [
-            'products',
-            'orders',
-            'users',
-            'events',
-            'messages',
-            'notices',
-            'contents',
-            'notes',
-            'shops',
-            'ui',
-            'settings',
-            'roles'
-        ],
-
-        // ecodocs: initialize the data
-        init: function (types) {
-            // by default load everything
-            types = (angular.isDefined(types)?(angular.isArray(types)?types:[types]):data.types);
-            this.initObjects(types);
-            this.initFires(types);
-            this.initArrays(types);
-        },
-
-        // ecodocs: initialize the container objects for each data type
-        initObjects: function(types) {
-            angular.forEach(types, function(type){
-                data[type] = {
-                    ref: new Firebase("https://mngr.firebaseio.com/"+type),
-                    fire: null,
-                    array: []
-                };
-            });
-        },
-
-        // ecodocs: initialize the $firebase bindings for each data type
-        initFires: function (types) {
-            angular.forEach(types, function(type){
-                if(data[type].ref){
-                    data[type].fire = $firebase(data[type].ref);
-                }
-            });
-        },
-
-        // ecodocs: initialize the arrays for each data type
-        initArrays: function (types) {
-            angular.forEach(types, function(type) {
-                if(data[type] && data[type].fire && angular.isFunction(data[type].fire.$on)){
-                    data[type].fire.$on('value', function(){ data.fireToArray(type); });
-                }
-            });
-        },
-
-        // ecodocs: convert a $firebase refernce to an array (callback for data[type].fire.$on('value', function)
-        fireToArray: function(type){
-            data[type].array = $filter('orderByPriority')(data[type].fire);
-            console.log(type+' loaded');
-        },
-
-        // creates a user profile for a given account
-        createUserProfile: function(account){
-        },
-
-        // loads the user profile for a given account
-        loadUserProfile: function(account){
-        },
-
-        // reset user login credentials
-        resetUserCredentials: function(){
-            data.user.credentials.email = '';
-            data.user.credentials.password = '';
-            data.user.credentials.passConfirm = '';
-            data.user.credentials.recover = false;
-        },
-
-        // check if the given email is associated with an existing account
-        userEmailExists: function(email){
-        },
-
-        // check if the given username is associated with an existing account
-        usernameExists: function(username){
         }
     };
-
-    // ecodocs: self-init()'ing service (gets called only once)
-    data.init();
 
 	return data;
 });
