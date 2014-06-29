f0cf21e70e452bb7cb85a23f6d90fb4a0fd4357b
irthos <irth03@gmail.com>
2014-06-28 18:24:26 -0700

    i
    o

---
+++ directive/views/ux/notify/notify.js
@@ -0,0 +1,14 @@
+angular.module('mngr').directive('notify', function() {
+	return {
+		restrict: 'E',
+		replace: true,
+		scope: {
+
+		},
+		templateUrl: 'directive/views/ux/notify/notify.html',
+		link: function(scope, element, attrs, fn) {
+
+
+		}
+	};
+});
