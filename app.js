2aefd0d323a5e5d83fc9121dbd5efc957c347001
Pat Long <plong00@gmail.com>
2014-07-02 11:23:26 -0700

    switched dataLoader to be promise-based so we can call login('active') only after the data is loaded

--- app.js
+++ app.js
@@ -23,20 +23,27 @@ angular.module('mngr').config(function($stateProvider, $urlRouterProvider) {
 
 });
 
-angular.module('mngr').run(function($rootScope, api, data, $firebase, Firebase, $filter, mngrSecureFirebase) {
+angular.module('mngr').run(function($rootScope, api, data, $q, $firebase, Firebase, $filter, mngrSecureFirebase) {
 	//Firebase.enableLogging(true);
     var dataLoader = {
         // ecodocs: initialize the data
         init: function (types) {
+            var defer = $q.defer();
             // by default load everything
             types = (angular.isDefined(types)?(angular.isArray(types)?types:[types]):data.types);
-            this.initObjects(types);
-            this.initFires(types);
-            this.initArrays(types);
+            dataLoader.initObjects(types).then(function(){
+                dataLoader.initFires(types).then(function(){
+                    dataLoader.initArrays(types).then(function(){
+                        defer.resolve(true);
+                    });
+                });
+            });
+            return defer.promise;
         },
 
         // ecodocs: initialize the container objects for each data type
         initObjects: function(types) {
+            var defer = $q.defer();
             angular.forEach(types, function(type){
                 data[type] = {
                     ref: new Firebase("https://mngr.firebaseio.com/"+type),
@@ -44,26 +51,48 @@ angular.module('mngr').run(function($rootScope, api, data, $firebase, Firebase, 
                     array: []
                 };
             });
+            defer.resolve(true);
+            return defer.promise;
         },
 
         // ecodocs: initialize the $firebase bindings for each data type
         initFires: function (types) {
+            var defer = $q.defer();
+            var firesInitted = {};
             angular.forEach(types, function(type){
                 if(data[type].ref){
+                    var fireInitted = $q.defer();
+                    firesInitted[type] = fireInitted.promise;
                     data[type].fire = $firebase(data[type].ref);
+                    data[type].fire.$on('loaded', function(){
+                        fireInitted.resolve(true);
+                    });
                 }
             });
+            $q.all(firesInitted).then(function(result){
+                defer.resolve(result);
+            });
+            return defer.promise;
         },
 
         // ecodocs: initialize the arrays for each data type
         initArrays: function (types) {
+            var defer = $q.defer();
+            var arraysInitted = {};
             angular.forEach(types, function(type) {
                 if(data[type] && data[type].fire && angular.isFunction(data[type].fire.$on)){
+                    var arrayInitted = $q.defer();
+                    arraysInitted[type] = arrayInitted.promise;
                     data[type].fire.$on('value', function(){
                         dataLoader.fireToArray(type);
+                        arrayInitted.resolve(true);
                     });
                 }
             });
+            $q.all(arraysInitted).then(function(result){
+                defer.resolve(result);
+            });
+            return defer.promise;
         },
 
         // ecodocs: convert a $firebase refernce to an array (callback for data[type].fire.$on('value', function)
@@ -75,7 +104,6 @@ angular.module('mngr').run(function($rootScope, api, data, $firebase, Firebase, 
     dataLoader.init([
         'products',
         'orders',
-        'users',
         'events',
         'messages',
         'notices',
@@ -84,11 +112,14 @@ angular.module('mngr').run(function($rootScope, api, data, $firebase, Firebase, 
         'shops',
         'ui',
         'settings',
-        'roles'
-    ]);
+        'roles',
+        'userAccounts',
+        'users'
+    ]).then(function(){
+        api.login('active');
+    });
 
-    api.login('active');
-
+    // ecodocs: mngrSecureFirebase testing...
     var up = $firebase(new Firebase('https://mngr.firebaseio.com/users/-JQZB3Pmi-yyABRG5x7B'));
     up.$on('loaded', function(){
         var securityTest = mngrSecureFirebase({name: 'test', access: ['customer', 'user']}, up);
