f7f4d393d061ebf307a7de60908beb97157954c9
Pat Long <plong00@gmail.com>
2014-07-04 18:48:38 -0700

    moving functions out of ui and into api

--- service/api.js
+++ service/api.js
@@ -101,7 +101,7 @@ angular.module('mngr').factory('api',function(data, models, ui, $q, mngrSecureFi
                 else if(!result.new && result.name && result.email){
                     // user profile loaded
                     api.loadData().then(function(){
-                        ui.loadState();
+                        api.loadState();
                     });
                 }
                 else if(result.new && result.linked){
@@ -336,6 +336,100 @@ angular.module('mngr').factory('api',function(data, models, ui, $q, mngrSecureFi
                 }
             });
             return defer.promise;
+        },
+
+        setWorkspace: function(workspace, component, params){
+            if(Object.keys(ui.workspace).indexOf(workspace)!==-1){
+                // load the component into the workspace
+                ui.workspace[workspace].component = component.name;
+
+                // reset any existing parameters in this workspace
+                angular.forEach(ui.workspace[workspace].params, function(value, name){
+                    delete ui.workspace[workspace].params[name];
+                });
+
+                // load the parameters we are given
+                if(angular.isArray(params)){
+                    // for array params - load the params in the order the component defines them
+                    angular.forEach(component.params, function(name, index){
+                        if(index < params.length){
+                            ui.workspace[workspace].params[name] = params[index];
+                        }
+                    });
+
+                    // if there are more params than component is configured for, append the extra params onto the last configured param
+                    if(params.length > component.params.length){
+                        var lastParam = component.params[component.params.length-1];
+                        ui.workspace[workspace].params[lastParam] += (ui.workspace[workspace].params[lastParam]?'/':'')+params.slice(component.params.length).join('/');
+                    }
+                }
+                else if(angular.isObject(params)){
+                    // for object params - set the params that are defined for the component
+                    angular.forEach(params, function(value, name){
+                        if(component.params.indexOf(name)!==-1){
+                            ui.workspace[workspace].params[name] = value;
+                        }
+                    });
+                }
+            }
+        },
+        loadPath: function(path, workspace){
+            if(Object.keys(ui.workspace).indexOf(workspace)!==-1){
+                // valid workspace, load the path into the ui
+
+                // strip off any leading or trailing /'s from path (for easier parsing)
+                path = (path.charAt(0) === '/') ? path.substr(1) : path;
+                path = (path.charAt(path.length - 1) === '/') ? path.substring(0, path.length - 1) : path;
+
+                // parse the path by /'s
+                var pathParts = path.split('/');
+                var componentFound = false;
+                if(pathParts && pathParts.length){
+                    var componentName = pathParts[0];   // component name is part before first '/'
+                    var params = pathParts.slice(1);    // params are everything after first '/'
+
+                    // search for the given componentName in the configured components
+                    // set it into the workspace when a match is found
+                    angular.forEach(ui.components, function(component, componentIdx){
+                        if(component.name===componentName){
+                            api.setWorkspace(workspace, component, params);
+                            componentFound = true;
+                        }
+                    });
+                    // component not found in ui.components - search in ui.notify.components
+                    if(!componentFound){
+                        angular.forEach(ui.notify.components, function(component, componentIdx){
+                            if(component.name===componentName){
+                                api.setWorkspace(workspace, component, params);
+                                componentFound = true;
+                            }
+                        });
+                    }
+                }
+
+                // if the first part of the path is not a valid component, load the default component
+                if(!componentFound){
+                    angular.forEach(ui.components, function(component, componentIdx){
+                        if(component.default){
+                            api.setWorkspace(workspace, component, pathParts);
+                            componentFound = true;
+                        }
+                    });
+                }
+                // at this point if !componentFound - then there isn't even a default component configured
+            }
+        },
+        loadState: function (fromPath, withParams) {
+            var path = angular.isDefined(fromPath) ? fromPath : ui.path;
+            var params = angular.isDefined(withParams) ? withParams : ui.params;
+
+            if(path){
+                api.loadPath(path, 'main');
+            }
+
+            angular.forEach(params, function(value, workspace){
+                api.loadPath(value, workspace);
+            });
         }
 
 	};
